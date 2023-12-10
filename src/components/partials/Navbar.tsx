68bafca0fa714d396a61c7b1fa8d8223a1686cc2
hakankaraahmet <karaahmethkn@gmail.com>
2023-12-10 20:40:39 +0100

    feat : navbar sidebar

--- src/components/partials/Navbar.tsx
+++ src/components/partials/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import Tooltip from "../ui/Tooltip";
 import Switcher from "../ui/Switcher";
 import { LuLogIn } from "react-icons/lu";
@@ -8,10 +8,47 @@ import { BsBasket3 } from "react-icons/bs";
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdNightlightRound } from "react-icons/md";
 import { IoSearch } from "react-icons/io5";
+import Typography from "../ui/Typography";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
   const [theme, setTheme] = useState<string>("light");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1);
 
+  //Searching (Dummy data will be changed with the exact data after connection of backend)
+  interface SearchResult {
+    id: number;
+    name: string;
+  }
+
+  const dummyData: SearchResult[] = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Smith" },
+    { id: 3, name: "Bob Johnson" },
+  ];
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    const results = dummyData.filter((item) =>
+      item.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setSearchResults(results);
+    setSelectedIndex(-1);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "ArrowDown" && selectedIndex < searchResults.length - 1) {
+      setSelectedIndex((prevIndex) => prevIndex + 1);
+    } else if (e.key === "ArrowUp" && selectedIndex > 0) {
+      setSelectedIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  //Dark Mode
   useEffect(() => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
@@ -29,19 +66,20 @@ const Navbar = () => {
     });
   };
 
-  console.log(theme);
   return (
     <nav className="border border-red-500  mb-8">
       <div className="flex justify-between items-center border-2 border-black py-8 px-12 bg-primary-800">
         <div>
           {" "}
-          <h1 className="text-secondary-400 font-bold text-3xl tracking-wider">
-            {" "}
-            <span className="">Tech </span>{" "}
-            <span className="text-tertiary">Store</span>
-          </h1>{" "}
+          <Link href={"/"}>
+            <h1 className="text-secondary-400 font-bold text-3xl tracking-wider">
+              {" "}
+              <span className="">Tech </span>{" "}
+              <span className="text-tertiary">Store</span>
+            </h1>{" "}
+          </Link>
         </div>
-        <div className="dark:text-white w-1/3 ">
+        <div className="dark:text-white w-1/3 relative ">
           {/* ---------------------------------------------------------------- */}
           <form className="relative">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -51,15 +89,45 @@ const Navbar = () => {
               type="search"
               className="block w-full  py-4  ps-10 pe-6 text-sm text-primary-800  rounded-full focus:outline-none"
               placeholder="What are you looking for?"
+              value={searchTerm}
+              onChange={handleSearch}
+              onKeyDown={handleKeyDown}
               required
             />
           </form>
+          {searchTerm && (
+            <div className="absolute flex flex-col gap-y-2 bg-secondary-300 dark:bg-primary-700 dark:border-secondary-300 top-16  border-2 border-primary-700 rounded-md p-2 w-full">
+              <Typography as="h3">
+                Search Results of{" "}
+                <span className="font-bold dark:font-bold">"{searchTerm}"</span>
+              </Typography>
+              {searchResults.map((result, index) => (
+                <Link
+                  href={"/asd"}
+                  key={result.id}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      setSearchTerm(result.name);
+                      router.push('/asd')
+                    }
+                  }}
+                  role="button"
+                  onClick={() => setSearchTerm(result.name)}
+                  className={`cursor-pointer text-primary-800 dark:text-secondary-400 ${
+                    index === selectedIndex ? "bg-primary-500 " : ""
+                  }`}
+                >
+                  {result.name}
+                </Link>
+              ))}
+            </div>
+          )}
           {/* ---------------------------------------------------------------- */}
         </div>
         <div className="flex gap-x-8">
           <Switcher
             leftIcon={
-              <MdNightlightRound className="text-primary-800  text-lg" />
+              <MdNightlightRound className="text-primary-800   text-lg" />
             }
             rightIcon={
               <MdOutlineLightMode className="  text-secondary-400   text-lg" />
--- src/components/ui/Switcher.tsx
+++ src/components/ui/Switcher.tsx
@@ -17,13 +17,13 @@ const Switcher = ({ leftIcon, rightIcon, onClick }: ISwitcher) => {
         onClick(e);
       }}
       className={`${
-        position ? "bg-primary-800" : "bg-secondary-400"
-      } border-2 border-secondary-400  w-20 rounded-full relative h-8 cursor-pointer`}
+        position ? "bg-primary-800" : "bg-secondary-400  "
+      } border-2 border-secondary-400  w-20 rounded-full relative h-8 cursor-pointer `}
     >
       <span
         className={`${
           position
-            ? "translate-x-11 bg-secondary-300  "
+            ? "translate-x-11 bg-secondary-300 scale-[85%]"
             : "translate-x-0 bg-primary-700 "
         } transition-transform duration-300 flex justify-center items-center ease-in-out transform  absolute  rounded-full w-[30px] h-[30px] top-1/2 translate-y-[-50%]  bottom-0`}
       >
